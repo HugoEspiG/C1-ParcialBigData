e55f02acda7773cd463ae1cf64faa2dd12959342
HugoEspiG <hugo.espinosa01@correo.usa.edu.co>
2023-08-31 00:01:59 -0500

    Update overlow + fixes

--- lambda_functions/download_lambda/scrapping_pages.py
+++ lambda_functions/download_lambda/scrapping_pages.py
@@ -1,22 +1,30 @@
-import json
 import boto3
 import urllib.request
+from datetime import datetime
 
-def lambda_handler_scrapping(event, context):
-    url = 'https://www.eltiempo.com/'
-    
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-    
-    s3_client = boto3.client('s3')
-    s3_client.put_object(
-        Body=html,
-        Bucket='bigdata2023hugoespinosa',
-        Key='index.html',
-        ContentType='text/html'
-    )
-    
+urls = {
+    "eltiempo": "https://www.eltiempo.com/",
+    "publimetro": "https://www.publimetro.co/"
+}
+
+# Configurar cliente de S3
+s3_client = boto3.client("s3")
+
+
+def lambda_handler(event, context):
+    # Obtener fecha actual
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    for newspaper, url in urls.items():
+        response = urllib.request.urlopen(url)
+        content = response.read()
+        # Definir la ruta en S3
+        s3_path = f"news/raw/{newspaper}-{current_date}.html"
+        # Subir contenido a S3
+        s3_client.put_object(Bucket="bigdata2023hugoespinosa",
+                             Key=s3_path,
+                             Body=content)
+        print(f"Contenido de {newspaper} guardado en S3: {s3_path}")
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': 'Contenido HTML guardado en S3 exitosamente'
     }
--- lambda_functions/processing_lambda/processing_data.py
+++ lambda_functions/processing_lambda/processing_data.py
@@ -1,14 +1,15 @@
 import json
 import boto3
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 
 def lambda_handler_processing(event, context):
     try:
         # Obtener información sobre el archivo cargado desde el evento
-        bucket_name = event['Records'][0]['s3']['bucket']['name']
-        object_key = event['Records'][0]['s3']['object']['key']
-
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        bucket_name = 'bigdata2023hugoespinosa'
+        object_key = f"news/raw/contenido-{current_date}.html"
         # Descargar contenido del archivo desde S3
         s3_client = boto3.client('s3')
         response = s3_client.get_object(
@@ -16,10 +17,8 @@ def lambda_handler_processing(event, context):
             Key=object_key
         )
         html_content = response['Body'].read().decode('utf-8')
-
         # Procesar contenido HTML con Beautiful Soup
         soup = BeautifulSoup(html_content, 'html.parser')
-
         # Extraer información de las noticias
         news_data = []
         for news_item in soup.find_all('div', class_='news'):
@@ -31,30 +30,25 @@ def lambda_handler_processing(event, context):
                 'title': title,
                 'link': link
             })
-
         # Crear contenido CSV
         csv_content = 'category,title,link\n'
         for news in news_data:
             csv_content += (
                 f'"{news["category"]}", "{news["title"]}", "{news["link"]}"\n'
             )
-
-
         # Subir el archivo CSV a S3
-        s3_key = (
-            f'headlines/final/'
-            f'{object_key.replace("raw/contenido-", "").replace(".html", ".csv")}'
-        )
+        s3_key = f'headlines/final/{object_key[13:-5]}.csv'
         s3_client.put_object(
             Body=csv_content,
             Bucket=bucket_name,
             Key=s3_key,
             ContentType='text/csv'
         )
-
         return {
             'statusCode': 200,
-            'body': json.dumps('Datos procesados y guardados en CSV exitosamente')
+            'body': json.dumps(
+                'Datos procesados y guardados en CSV exitosamente'
+            )
         }
     except Exception as e:
         return {
